a170eb4ec29567ecbd4ea92307df3a710f054059
ong-ar <reclusis@gmail.com>
2019-08-05 16:11:14 +0900

    text stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,20 +21,25 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(children: <Widget>[
           Container(
-            margin: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10.0),
             child: RaisedButton(
               onPressed: () {},
               child: Text('Add Product'),
             ),
           ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/food.jpeg'),
-                Text('Food Paradise')
-              ],
-            ),
-          ),
+          Column(
+              children: _products
+                  .map(
+                    (element) => Card(
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset('assets/food.jpeg'),
+                          Text(element)
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList())
         ]),
       ),
     );
